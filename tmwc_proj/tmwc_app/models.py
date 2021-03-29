5ae2287c7cd3d80a2e8741b2ec6942e77c46b6c7
Eric <eric.vasquez3327@gmail.com>
2021-03-29 15:30:55 -0500

    added placeholders for txt and imgs

--- tmwc_proj/tmwc_app/models.py
+++ tmwc_proj/tmwc_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+import re
 # Create your models here.
+class UserManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['first_name']) < 2 or len(postData['first_name']) > 50:
+            errors["first_name"] = "First Name must be between 2 and 50 characters long"
+
+        if len(postData['last_name']) < 2 or len(postData['last_name']) > 50:
+            errors["last_name"] = "Last Name must be between 2 and 50 characters long"
+
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):              
+            errors['email'] = "Invalid email address!"
+
+        if len(postData['password']) < 8:
+            errors["password"] = "Password must be at least 8 characters long"
+
+        if postData['password'] != postData['confirmPassword']:
+            errors["confirmPassword"] = "Password and Confirm Password do not match"            
+        return errors
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
--- tmwc_proj/tmwc_app/views.py
+++ tmwc_proj/tmwc_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from time import gmtime, strftime
+from django.contrib import messages
+import bcrypt
+from .models import User
 # Create your views here.
+
+def index(request):
+    return render(request, "index.html")
